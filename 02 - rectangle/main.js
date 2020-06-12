427ecd351229aec2696526ce778105bad72ffe51
M.Alghobary <m.alghobari.ys@gmail.com>
2020-06-12 16:59:19 +0300

    use less vertexes with index buffer

--- 02 - rectangle/main.js
+++ 02 - rectangle/main.js
@@ -4,30 +4,21 @@ const webgl = gl._gl;
 
 // create shader program
 const program = gl.createProgramFromSource('vertex-shader-src', 'fragment-shader-src');
-gl.useProgram();
 
 // get the program attribute locations
 const posLocation = gl.getLocation(gl.ATTR, 'a_position');
 const colorLocation = gl.getLocation(gl.UNIFORM, 'u_color');
 
 // create the position buffer
-const posData = new Float32Array([
-	// first triangle
-	-0.5,
-	-0.5,
-	0.5,
-	-0.5,
-	-0.5,
-	0.5,
-	// first triangle
-	0.5,
-	-0.5,
-	0.5,
-	0.5,
-	-0.5,
-	0.5,
-]);
+const posData = new Float32Array([-0.5, 0.5, 0.5, 0.5, 0.5, -0.5, -0.5, -0.5]);
 const posBuffer = gl.createBuffer(posData);
+
+// create the index buffer
+const indexData = new Uint16Array([0, 1, 2, 2, 3, 0]);
+const indexBuffer = gl.createBuffer(indexData, gl.INDEX_BUFFER);
+
+// set the current program
+gl.useProgram();
 
 // define position attribute
 webgl.vertexAttribPointer(posLocation, 2, webgl.FLOAT, false, 0, 0);
@@ -46,7 +37,7 @@ function draw() {
 	gl.clear(130, 140, 250);
 
 	// draw the triangle
-	webgl.drawArrays(webgl.TRIANGLES, 0, 6);
+	webgl.drawElements(webgl.TRIANGLES, indexData.length, webgl.UNSIGNED_SHORT, 0);
 }
 
 draw();
