aedb7ada819476b8bbe863b8189e5b8154381e77
M.Alghobary <m.alghobari.ys@gmail.com>
2020-06-12 16:41:28 +0300

    make rectangle from two triangles

---
+++ 02 - rectangle/main.js
@@ -0,0 +1,52 @@
+// init webgl
+const gl = new GL('canvas');
+const webgl = gl._gl;
+
+// create shader program
+const program = gl.createProgramFromSource('vertex-shader-src', 'fragment-shader-src');
+gl.useProgram();
+
+// get the program attribute locations
+const posLocation = gl.getLocation(gl.ATTR, 'a_position');
+const colorLocation = gl.getLocation(gl.UNIFORM, 'u_color');
+
+// create the position buffer
+const posData = new Float32Array([
+	// first triangle
+	-0.5,
+	-0.5,
+	0.5,
+	-0.5,
+	-0.5,
+	0.5,
+	// first triangle
+	0.5,
+	-0.5,
+	0.5,
+	0.5,
+	-0.5,
+	0.5,
+]);
+const posBuffer = gl.createBuffer(posData);
+
+// define position attribute
+webgl.vertexAttribPointer(posLocation, 2, webgl.FLOAT, false, 0, 0);
+
+// turn on the position attribute
+webgl.enableVertexAttribArray(posLocation);
+
+// pass the color
+webgl.uniform3f(colorLocation, 1, 1, 1);
+
+function draw() {
+	// rezie canvas
+	gl.reSize();
+
+	// clear background color
+	gl.clear(130, 140, 250);
+
+	// draw the triangle
+	webgl.drawArrays(webgl.TRIANGLES, 0, 6);
+}
+
+draw();
